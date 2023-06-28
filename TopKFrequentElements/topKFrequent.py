d6e4d8064b33951e95c46db33db809c7ed335b5e
Changwhi <whinada@gmail.com>
2023-06-28 16:24:07 -0700

    Runtime 108ms Memory 21MB 2hrs

--- TopKFrequentElements/topKFrequent.py
+++ TopKFrequentElements/topKFrequent.py
@@ -16,7 +16,6 @@ class Solution(object):
         # while the values correspond to the numbers in the given array.
         frequent = [[] for i in range(len(nums)+1)]
 
-
         for i in range(len(nums)):
             value_frequency[nums[i]] = 1 + value_frequency.get(nums[i], 0)
         for key, value in value_frequency.items():
@@ -24,8 +23,9 @@ class Solution(object):
             frequent[value].append(key)
 
         result = []
-        for index in range(len(frequent) -1, -1, -1):
-            for number in frequent[index]:  # to handle a situation that contains more than 2 value
+        for index in range(len(frequent) - 1, -1, -1):
+            for number in frequent[index]:
+                # to handle a situation that contains more than 2 value
                 result.append(number)
                 if len(result) == k:
                     return result
