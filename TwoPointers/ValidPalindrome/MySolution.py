e1901eb90281ef245489f0b0f7c0184693f85093
Changwhi <whinada@gmail.com>
2023-07-17 09:44:05 -0700

    success 339ms 5.32%  16.2 9.30%

---
+++ TwoPointers/ValidPalindrome/MySolution.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        # convert each character into lowercase
+        # convert each character into ACSII Code
+        # check if each character is alphanumeric
+        # if it is alphanumeric, store it into an array.
+        # check if it is a palindrome
+
+
+        converted = ""
+        for char in s:
+            lowerchar = char.lower()
+            if ord(lowerchar) in range(48, 58) or ord(lowerchar) in range(97, 123):
+                converted += lowerchar
+
+        if len(converted) == 0:
+            return True
+
+        for pointer1 in range(len(converted)):
+            if converted[pointer1] != converted[(pointer1+1) * -1]:
+                return False
+        return True
---
+++ TwoPointers/ValidPalindrome/Solution2.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        # convert each character into lowercase
+        # convert each character into ACSII Code
+        # check if each character is alphanumeric
+        # if it is alphanumeric, store it into an array.
+        # check if it is a palindrome
+
+        result = ''.join(c for c in s.lower() if c.isalnum())
+        return result==result[::-1]
+
+
+        # result = ""
+        # for character in s:
+        #     if character.isalnum():
+        #         result += character.lower()
+        # reversed = result[::-1]
+        # return result == reversed
+
+
+        # converted = ""
+        # for char in s:
+        #     lowerchar = char.lower()
+        #     if ord(lowerchar) in range(48, 58) or ord(lowerchar) in range(97, 123):
+        #         converted += lowerchar
+
+        # if len(converted) == 0:
+        #     return True
+
+        # for pointer1 in range(len(converted)):
+        #     if converted[pointer1] != converted[(pointer1+1) * -1]:
+        #         return False
+        # return True
