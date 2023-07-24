33fa018a0fd0f7e64049ea27bb79f8d39b817aa5
Changwhi <whinada@gmail.com>
2023-07-24 16:46:17 -0700

    Success Runtime: 137ms[75.49%] Memory: 17.2MB[61.76%]

---
+++ TwoPointers/TwoSum2/MySolution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        basket = {}
+
+        for index, value in enumerate(numbers):
+            requiredSecondValue = target - value
+
+            if requiredSecondValue in basket:
+                return (basket[requiredSecondValue] + 1, index + 1)
+            basket[value] = index
+        # using dictionary instead of two pointer
